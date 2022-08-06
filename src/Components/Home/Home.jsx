4bf5382f0c57feb0cd62abb16ca2ec3f392f27a8
iti tripathy <iti.tripathy@gmail.com>
2022-08-06 12:31:11 +1000

    Home + Nav  Component

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import image from "./../../Images/Home_Background.jpg";
+
+const Home = () => {
+    return (
+        <>
+            <div>
+                <h1>Welcome</h1>
+                <img src={image}></img>
+            </div>
+        </>
+    );
+};
+
+export default Home;
---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./Nav.module.scss";
+import { NavLink } from "react-router-dom";
+import Home from "./../Home/Home";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Nav = () => {
+    return (
+        <>
+            <nav className={styles.Nav}>
+                <NavLink to="/" className={styles.NavLink}>
+                    Home
+                </NavLink>
+                <NavLink to="/Shop" className={styles.NavLink}>
+                    Shop
+                </NavLink>
+                <NavLink to="/Cart" className={styles.NavLink}>
+                    <span>
+                        <FontAwesomeIcon icon="fa-solid fa-shop" />
+                    </span>
+                    Shopping Cart
+                </NavLink>
+            </nav>
+        </>
+    );
+};
+
+export default Nav;
